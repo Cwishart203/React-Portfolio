bc0773ecfa2c667a99e8e14ffd7d7e5250c44646
Cwishart203 <93050589+Cwishart203@users.noreply.github.com>
2022-01-26 13:21:31 -0500

    Added links to project pictures

--- src/component/projects/projects.js
+++ src/component/projects/projects.js
@@ -5,6 +5,7 @@ import reviewCoreSS from "../../img/reviewCoreSS.png"
 import codeQuizSS from "../../img/codeQuizSS.png"
 import passGenSS from "../../img/passGenSS.png"
 import dayPlannerSS from "../../img/dayPlannerSS.png"
+import noteTakerSS from "../../img/noteTakerSS.png"
 
 function Projects() {
     return (
@@ -12,8 +13,8 @@ function Projects() {
             <h2 className= "page-title">Portfolio</h2>
             <div className="row">
                 <div className= "projects col-md-4">
-                    <img src={pokeFinderSS}/>
                     <a href="https://bchen41.github.io/PokeFinder/">
+                        <img src={pokeFinderSS}/>
                         <h3 class="project-title">
                             PokeFinder
                         </h3>
@@ -25,8 +26,8 @@ function Projects() {
                     </a>
                 </div>
                 <div className= "projects col-md-4">
-                    <img src={reviewCoreSS}/>
                     <a href="https://review-core.herokuapp.com/index.html">
+                        <img src={reviewCoreSS}/>
                         <h3 class="project-title">
                             Review-Core
                         </h3>
@@ -38,8 +39,8 @@ function Projects() {
                     </a>
                 </div>
                 <div className= "projects col-md-4">
-                    <img src={codeQuizSS}/>
                     <a href="https://cwishart203.github.io/Code-Quiz/">
+                        <img src={codeQuizSS}/>
                         <h3 class="project-title">
                             Code-Quiz
                         </h3>
@@ -51,8 +52,8 @@ function Projects() {
                     </a>
                 </div>
                 <div className= "projects col-md-4">
-                    <img src={passGenSS}/>
                     <a href="https://cwishart203.github.io/password-generator/">
+                        <img src={passGenSS}/>
                         <h3 class="project-title">
                             Password Generator
                         </h3>
@@ -64,13 +65,26 @@ function Projects() {
                     </a>
                 </div>
                 <div className= "projects col-md-4">
-                    <img src={dayPlannerSS}/>
-                    <a href="https://cwishart203.github.io/password-generator/">
+                    <a href="https://cwishart203.github.io/Day-Planner/">
+                        <img src={dayPlannerSS}/>
                         <h3 class="project-title">
                             Day Planner
                         </h3>
                     </a>
                     <a href="https://github.com/Cwishart203/Day-Planner">
+                        <h3 class="project-title">
+                            Github
+                        </h3>
+                    </a>
+                </div>
+                <div className= "projects col-md-4">
+                    <a href="https://note-taker-cwish.herokuapp.com/">
+                        <img src={noteTakerSS}/>
+                        <h3 class="project-title">
+                            Note Taker
+                        </h3>
+                    </a>
+                    <a href="https://github.com/Cwishart203/Note-Taker">
                         <h3 class="project-title">
                             Github
                         </h3>
