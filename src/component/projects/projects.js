9ffed1fdc2e626b36c00aa8c40c64a80cecf5682
Cwishart203 <93050589+Cwishart203@users.noreply.github.com>
2022-01-26 11:20:42 -0500

    Added github links and pictures to Projects

--- src/component/projects/projects.js
+++ src/component/projects/projects.js
@@ -1,5 +1,8 @@
 import React from "react";
-import "./projects.css"
+import "./projects.css";
+import pokeFinderSS from "../../img/pokeFinderSS.png"
+import reviewCoreSS from "../../img/reviewCoreSS.png"
+import codeQuizSS from "../../img/codeQuizSS.png"
 
 function Projects() {
     return (
@@ -7,23 +10,41 @@ function Projects() {
             <h2 className= "page-title">Portfolio</h2>
             <div className="row">
                 <div className= "projects col-md-4">
+                    <img src={pokeFinderSS}/>
                     <a href="https://bchen41.github.io/PokeFinder/">
                         <h3 class="project-title">
                             PokeFinder
                         </h3>
                     </a>
+                    <a href="https://github.com/bchen41/PokeFinder">
+                        <h3 class="project-title">
+                            Github
+                        </h3>
+                    </a>
                 </div>
                 <div className= "projects col-md-4">
+                    <img src={reviewCoreSS}/>
                     <a href="https://review-core.herokuapp.com/index.html">
                         <h3 class="project-title">
                             Review-Core
                         </h3>
                     </a>
+                    <a href="https://github.com/SteveKoutsodontis/Review-Core">
+                        <h3 class="project-title">
+                            Github
+                        </h3>
+                    </a>
                 </div>
                 <div className= "projects col-md-4">
+                    <img src={codeQuizSS}/>
                     <a href="https://cwishart203.github.io/Code-Quiz/">
                         <h3 class="project-title">
                             Code-Quiz
+                        </h3>
+                    </a>
+                    <a href="https://github.com/Cwishart203/Code-Quiz">
+                        <h3 class="project-title">
+                            Github
                         </h3>
                     </a>
                 </div>
