674e81a12112f683e16f78dcde85e98ed15a03df
Cwishart203 <93050589+Cwishart203@users.noreply.github.com>
2022-01-26 13:55:48 -0500

    Contact page update

--- src/component/contact/contact.js
+++ src/component/contact/contact.js
@@ -6,7 +6,47 @@ function Contact() {
         <div>
             <div className="contact">
                 <h2>Contact Me Below</h2>
-                <p>knfanlgflkagtnklgnaa</p>
+                <form>
+                    <div className="row">
+                        <div className="col-md-4">
+                            <label for="name">Name</label>
+                                <br />
+                                <input
+                                    type="name"
+                                    id="name"
+                                    name="name"
+                                    class="form-control"
+                                ></input> 
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-4">
+                            <label for="email">Email</label>
+                            <br />
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                class="form-control"
+                            ></input>
+                        </div>
+                        
+                    </div>
+                    <div className="row">
+                        <div className="col-md-8">
+                            <label for="message">Type your message here</label>
+                            <br />
+                            <textarea
+                                type="message"
+                                id="message"
+                                name="message"
+                                class="form-control"
+                            ></textarea>
+                        </div>
+                        
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         </div>
     )
