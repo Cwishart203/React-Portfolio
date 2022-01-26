cafe602d2273a2426d13556b6fc8c7178b020f3e
Cwishart203 <93050589+Cwishart203@users.noreply.github.com>
2022-01-26 08:54:24 -0500

    About section placeholder

---
+++ src/component/about/about.js
@@ -0,0 +1,25 @@
+import React from "react"
+import "./about.css"
+
+function About() {
+    return (
+        <div className="about">
+            <div className="row">
+                <div className="col-md-6 about-picture">
+
+                </div>
+                <div className="col-md-6 about-body">
+                    <h2>About Me</h2>
+                    <p>Hi, I'm Chris Wishart. I'm a 29 year old aspiring web developer from Medford, New Jersey.
+                My hobbies include gaming, reading, writing short stories, exercising, having philosophical debates,
+                and occasionally running D&D for my friends. Originally studied liberal arts education and philosophy, but
+                am now trying my hand at the art of coding and web development.</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+
+export default About
---
+++ src/pages/Aboutme.js
@@ -0,0 +1,12 @@
+import React from "react";
+import About from "../component/about/about";
+
+function Aboutme() {
+    return (
+        <div>
+            < About />
+        </div>
+    );
+}
+
+export default Aboutme
