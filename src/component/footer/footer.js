2bc09be37cb64e73f74f4a78a9e7322f89868658
Cwishart203 <93050589+Cwishart203@users.noreply.github.com>
2022-01-26 12:18:46 -0500

    Changed footer links to images

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -1,5 +1,8 @@
 import React from "react"
 import "./footer.css"
+import github from "../../img/github.png"
+import linkedIn from "../../img/linkedIn.png"
+import gmail from "../../img/gmail.png"
 
 function Footer() {
     return (
@@ -9,23 +12,17 @@ function Footer() {
                     <ul>
                         <li>
                             <a href="https://github.com/Cwishart203">
-                                <h3 class="footer-link">
-                                    Github
-                                </h3>
+                                <img src={github}/>
                              </a>
                         </li>
                         <li>
                             <a href="https://www.linkedin.com/in/chris-wishart-a5baa4223/">
-                                <h3 class="footer-link">
-                                    LinkedIn
-                                </h3>
+                                <img src={linkedIn}/>
                             </a>
                         </li>
                         <li>
                             <a href="mailto:cwishart203@gmail.com">
-                                <h3 class="footer-link">
-                                    Gmail
-                                </h3>
+                                <img src={gmail}/>
                             </a>
                         </li>
                     </ul>
